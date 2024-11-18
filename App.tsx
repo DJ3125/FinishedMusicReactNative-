e678394134923fac3da9191d5e6a8d06026854d1
DJ3125 <dylanalexja@gmail.com>
2024-11-17 20:14:07 -0600

    finished product

--- App.tsx
+++ App.tsx
@@ -1,9 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable} from 'react-native';
+// import {useEffect} from "react";
+// import Sound from "react-native-sound";
+
+import {Audio} from "expo-av";
+
+// impor 
+
+// const sound = new Sound("./assets/correctRound.mp3");
+
+// function playSound():void{
+//   sound.play();
+// }
+
+
 
 export default function App() {
+  const audio = async function(){
+    const {sound} = await Audio.Sound.createAsync(require("./assets/correctRound.mp3"));
+    await sound.playAsync();
+  }
+
+
   return (
     <View style={styles.container}>
+      <Pressable onPress={audio}><Text>Press me</Text></Pressable>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
